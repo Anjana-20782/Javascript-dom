7c3136f3449b8602922beb4c819f2d563bb6f96c
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-24 20:57:15 +0530

    new contents added

--- js/index.js
+++ js/index.js
@@ -3,3 +3,19 @@
 //    alert("hai")
 // }
 
+// function handlekey()
+// {
+//     let x=document.getElementById("name").value;
+//     console.log(x);
+    
+// }
+
+// function handlekey(e)
+// {
+//     console.log(e.value);
+    
+// }
+
+
+
+
