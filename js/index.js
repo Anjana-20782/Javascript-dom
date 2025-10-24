5c7f9d3e1e257d2917e80938bd4dcc89ea9eb9df
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-24 20:53:52 +0530

    new contents added

---
+++ js/index.js
@@ -0,0 +1,67 @@
+// function formsbmt(){
+
+//    alert("hai")
+// }
+
+// function handlekey()
+// {
+//     let x=document.getElementById("name").value;
+//     console.log(x);
+    
+// }
+
+// function handlekey(e)
+// {
+//     console.log(e.value);
+    
+// }
+
+
+
+
+// function formsbmt()
+// {
+//     // let a=document.getElementById("name").value;
+//     // let b=document.getElementById("email").value;
+//      let a=parseInt(document.getElementById("name").value);
+//     let b=parseInt(document.getElementById("email").value);
+//     let c=a+b
+//     console.log(c);
+//     let res=document.getElementById("result").textContent=c;
+//     // let res=document.getElementById("result").innerHTML=`<p>hai</p>`;
+//     // let res=document.getElementById("result").textContent=`<p>hai</p>`;
+    
+// }
+
+
+
+
+// document.getElementById("clk").addEventListener(`click`,()=>{
+
+//     let x=parseInt(document.getElementById("number").value);  
+//     let str=``
+//     for(i=1;i<=x;i++)
+//         {
+
+//             str+=`<li>${i}</li>`
+//         }  
+//         document.getElementById("list").innerHTML=str
+// })
+
+
+
+
+
+ document.getElementById("clk").addEventListener(`click`,()=>{
+
+    let x=parseInt(document.getElementById("number").value);
+
+    for(i=1;i<=x;i++){
+       let a= document.createElement("li")
+       a.innerHTML=i;
+       a.style.color="red"
+       a.style.listStyle="none"
+       document.getElementById("list").appendChild(a)
+    }
+   
+ }) 
